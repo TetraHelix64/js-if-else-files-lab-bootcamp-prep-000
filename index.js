80aaf1e4c1d77871450cb895f8949f682eed06c5
Josiah Furrer <jbfjosiah@gmail.com>
2017-10-01 07:13:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-var name = "Joe"
+var name = "Susan"
 if (name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected the name to be Susan")
 } //if (console.log.innerTEXT === "Expected the name to be Susan") {var name = "Susan"}
 
-var height = "74"
-var heightSelector = document.querySelector('#height')
-	heightSelector.innerHTML = "74"
-		heightSelector.innerHTML = parseInt(friendSelector.innerHTML) - 10
+var height = "70"
+
 // Don't worry about this
 module.exports = { name, height
 }
